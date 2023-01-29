5ade2237e0ce7b4a9bd7094b7727b238f50b9590
naumovanatalia <naumova.na43@mail.ru>
2023-01-29 19:25:15 +0200

    add readme file

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿Console.WriteLine("Введите текст");
-string [] array = new string [3];
-for(int i = 0; i < array.Length; i++)
-    {
-        array[i] = Console.ReadLine();
-    }
+string[] array = new string[3];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Console.ReadLine();
+}
 Console.WriteLine($"{string.Join(", ", array)}");
 int count = 0;
-for(int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++)
+{
+    string s = array[i];
+    if (s.Length < 4)
     {
-        string s = array[i];
-        if (s.Length < 4)
-        {
-            count++;
-            Console.Write($"{s} ");
-        }
+        count++;
+        Console.Write($"{s} ");
     }
-if (count == 0);
+}
+if (count == 0)
 {
     Console.WriteLine("null");
 }
