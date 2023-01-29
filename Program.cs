e28bda3810eba3595d328af41e5b441c2d01dc20
naumovanatalia <naumova.na43@mail.ru>
2023-01-29 18:24:54 +0200

    add condition all strings > 3 symvbols

--- Program.cs
+++ Program.cs
@@ -5,12 +5,17 @@ for(int i = 0; i < array.Length; i++)
         array[i] = Console.ReadLine();
     }
 Console.WriteLine($"{string.Join(", ", array)}");
-
+int count = 0;
 for(int i = 0; i < array.Length; i++)
     {
         string s = array[i];
         if (s.Length < 4)
         {
+            count++;
             Console.Write($"{s} ");
         }
     }
+if (count == 0);
+{
+    Console.WriteLine("null");
+}
