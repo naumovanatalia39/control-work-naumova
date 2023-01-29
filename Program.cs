6ab9c9e5ec31f621f3f7fee85a5cf87bc8c8fb8a
naumovanatalia <naumova.na43@mail.ru>
2023-01-29 18:11:44 +0200

    1version solution

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ for(int i = 0; i < array.Length; i++)
         array[i] = Console.ReadLine();
     }
 Console.WriteLine($"{string.Join(", ", array)}");
+
+for(int i = 0; i < array.Length; i++)
+    {
+        string s = array[i];
+        if (s.Length < 4)
+        {
+            Console.Write(s);
+        }
+    }
